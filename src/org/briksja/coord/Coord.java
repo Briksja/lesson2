b48f73cb8426385c5dbb56249d323a6fc536a91d
Briksja <briksja@gmail.com>
2017-12-16 17:44:31 +0100

    niech się stanie porządek

--- src/org/briksja/coord/Coord.java
+++ src/org/briksja/coord/Coord.java
@@ -9,7 +9,36 @@ public class Coord {
         this.y = y;
     }
 
+    public Coord() {
+        this.x = 0;
+        this.y = 0;
+    }
+
     public double sum() {
         return x + y;
     }
+
+    public static void ab(Coord c) {
+        c.x = 1.;
+        c.y = -1.;
+    }
+
+    public double multisum(double multi) {
+        double value = sum() * multi;
+        return value;
+    }
+
+    public double multisum(double multi1, double multi2) {
+        double value = sum() * multi1 * multi2;
+        return value;
+    }
+
+    public String toString() {
+        return "Coord " + x + " " + y;
+
+    }
+
 }
+
+
+
